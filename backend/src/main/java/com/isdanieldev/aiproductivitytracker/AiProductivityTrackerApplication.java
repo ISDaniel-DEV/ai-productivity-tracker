89f5132630beee1f5c02da6978699ec0bae25c08
ISDaniel-DEV <sdaniel9391@gmail.com>
2026-08-23 12:03:03 +0200

    backend created - first commit

---
+++ backend/src/main/java/com/isdanieldev/aiproductivitytracker/AiProductivityTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.isdanieldev.aiproductivitytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProductivityTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiProductivityTrackerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/isdanieldev/aiproductivitytracker/AiProductivityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isdanieldev.aiproductivitytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProductivityTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
